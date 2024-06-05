2cf539eae9e6c5fb91783964c721e70113cbf589
TasnimZaid <tasnim.aboarqob@gmail.com>
2024-06-05 04:01:00 +0300

    task1,2

--- app.js
+++ app.js
@@ -22,4 +22,40 @@ function submitOrder() {
     document.getElementById('message').innerText = message;
 }
 
-
+//  var name = prompt("name");
+//  var gender  = prompt("gender ") ;
+ 
+//  if (gender == "female"){
+//      alert("MS." + name) ;
+//  }
+//  else if( gender == "male") { 
+//      alert("Mr." + name) ;
+//  }
+//  else{
+//      while(gender != "female" || gender != "male"){
+//          gender = prompt("wrong in gender , please tery again");
+//          if (gender == "female"){
+//              alert("MS." + name) ;
+//          }
+//          else if( gender == "male") { 
+//              alert( welcoming = "Mr." + name) ;
+//           }
+//  }
+ 
+//  }
+ 
+//  var order = prompt('Choose from menue (Donut/Coffee/Ice Cream/Bakery)?');
+//  var item = prompt('Enter the name of your order:');
+ 
+//  console.log (order , item );
+ 
+ 
+ 
+//  const answerarray= [];
+//  answerarray.push(name , gender ,order ,item );
+//  console.log(answerarray);
+ 
+ 
+ 
+ 
+ 
